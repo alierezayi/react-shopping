483aa9fc9abd8fe249fea2ae67fe803ac6612bc5
Ali Rezaei <im1.bitcode@gmail.com>
2023-03-12 10:38:34 +0330

    edit sidebar

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,14 +1,17 @@
-import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowLeftOnRectangleIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
 import Link from "next/link";
 import Image from "next/image";
 import PopoverBox from "../cart/PopoverBox";
 import shopImage from "../../public/images/shop-image.png";
 import { Bars3Icon } from "@heroicons/react/24/outline";
 
-const Header = ({ items, isOpen, setIsOpen }) => {
+const Header = ({ items, setIsOpen }) => {
   return (
-    <header className="sticky top-0 z-10 w-full backdrop-blur border-b bg-white/70 border-b-slate-100 px-4 sm:px-8 lg:px-16 ">
-      <nav className="flex h-16 lg:h-20 justify-between items-center border-b">
+    <header className="sticky top-0 z-10 w-full backdrop-blur border-b bg-white/70 px-4 border-b-slate-100/80 sm:px-8 lg:px-16 ">
+      <nav className="flex h-16 lg:h-20 justify-between items-center border-b border-b-slate-100 lg:border-none">
         <Link href="/" className="text-lg font-bold">
           <Image src={shopImage} width={45} height={45} alt="main icon" />
         </Link>
@@ -32,9 +35,13 @@ const Header = ({ items, isOpen, setIsOpen }) => {
           </Link>
         </div>
       </nav>
-      <div className="h-11 flex justify-start items-center space-x-3">
-        <Bars3Icon className="w-8 h-8 text-gray-600" onClick={() => setIsOpen(true)} />
+      <div className="h-11 flex justify-between items-center space-x-3 lg:hidden">
+        <Bars3Icon
+          className="w-8 h-8 text-gray-600"
+          onClick={() => setIsOpen(true)}
+        />
         {/* <span className="text-gray-600">Home</span> */}
+        <MagnifyingGlassIcon className="w-6 h-6 text-gray-600" />
       </div>
     </header>
   );
