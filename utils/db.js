36018d04c1e77190d25c095b7833fb9d8e1360c5
Ali Rezaei <im1.bitcode@gmail.com>
2023-03-14 16:03:35 +0330

    insert to database

--- utils/db.js
+++ utils/db.js
@@ -1,25 +1,9 @@
 import mongoose from "mongoose";
 
 const connect = async () => {
-  await mongoose.connect(
-    "mongodb+srv://im1bitcode:VJCKryqD58R9mrFV@cluster0.q2bwqld.mongodb.net/?retryWrites=true&w=majority/shopping"
-  );
+  await mongoose.connect("mongodb://127.0.0.1:27017/shopping");
   console.log("Connected.");
 };
 
 const db = { connect };
 export default db;
-
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-// const uri =
-//   "mongodb+srv://im1bitcode:<password>@cluster0.q2bwqld.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   serverApi: ServerApiVersion.v1,
-// });
-// client.connect((err) => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
