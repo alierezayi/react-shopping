bf4f0fd039cca949474cf53a37fde38047c9dbad
Ali Rezaei <im1.bitcode@gmail.com>
2023-03-25 02:24:22 +0330

    complete banner

--- components/products/ProductGrid.js
+++ components/products/ProductGrid.js
@@ -1,9 +1,14 @@
 import Product from "./ProductItem";
 import productItems from "../../data/products.json";
 
-const ProductGrid = () => {
+const ProductGrid = ({ scrollRef }) => {
   return (
-    <div className="container m-auto px-1 mt-10" data-aos="fade-up" data-aos-duration="1000">
+    <div
+      className="container m-auto px-1 mt-10"
+      ref={scrollRef}
+      data-aos="fade-up"
+      data-aos-duration="1000"
+    >
       <h1 className="text-3xl border-b px-3 py-6 font-bold mb-6">Products</h1>
       <div className="rounded-2xl divide-y-14 md:px-20 mx-2 grid px-10 sm:grid-cols-2 gap-12 lg:gap-14 2xl:gap-20 lg:grid-cols-3 xl:grid-cols-4 md:grid-cols-2">
         {productItems.map((pItem) => (
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,52 @@
 import Image from "next/image";
 import Layout from "../components/layout/Layout";
 import ProductGrid from "../components/products/ProductGrid";
-import img from "../public/images/jezael-melgoza.jpg";
+import lgImage from "../public/images/banner/jezael-melgoza-1.jpg";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { ChevronDoubleDownIcon } from "@heroicons/react/24/outline";
 
 function Home() {
   useEffect(() => {
     Aos.init();
     Aos.refresh();
   }, []);
+
+  const scrollRef = useRef(null);
+
+  const executeScroll = () => scrollRef.current.scrollIntoView();
+
   return (
     <Layout title="Home page">
-      <div className="relative">
-        <div className="absolute w-full h-full bg-black/60 backdrop-blur-sm" />
-        <div
-          className="absolute top-[8%] left-[5%] lg:left-[15%] lg:right-[15%] right-[5%] space-y-3 xl:space-y-20"
-          data-aos="zoom-in"
-        >
-          <h1 className=" text-white text-3xl sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl font-bold">
-            Bit Code Shop
-          </h1>
-          <p className="xl:w-3/4 text-white text-sm sm:text-base md:text-lg px-10 lg:text-2xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
-          </p>
-        </div>
+      <div className="relative" data-aos="fade-up">
+        <div className="absolute w-full h-full bg-gradient-to-b from-slate-900/90 to-white/10 backdrop-blur-[3px] sm:backdrop-blur-sm" />
         <Image
-          src={img}
+          src={lgImage}
           width={700}
           height={350}
           alt="bg-shop"
-          className="h-[300px] md:h-[400px] lg:h-auto w-full"
+          className="h-[550px] md:h-[600px] lg:h-[750px] 2xl:h-[890px] w-full"
         />
+        <div className="absolute inset-0 space-y-3 flex justify-center items-center">
+          <div className="w-4/5 h-3/4 space-y-9 text-center">
+            <h1 className=" text-white text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold">
+              Bit Code Shop
+            </h1>
+            <p className="text-white sm:text-base px-5 md:text-lg lg:text-2xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+          <button
+            className="absolute bottom-16 cursor-pointer"
+            onClick={executeScroll}
+          >
+            <ChevronDoubleDownIcon className=" w-12 lg:w-20 text-blue-800" />
+          </button>
+        </div>
       </div>
-      <ProductGrid />
+      <ProductGrid scrollRef={scrollRef} />
     </Layout>
   );
 }
